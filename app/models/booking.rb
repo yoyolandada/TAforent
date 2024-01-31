0fdcccfe40de1ad30e8800c91c36020e383cf1a1
yolanda martinez priego <yulielx@gmail.com>
2024-01-31 13:59:11 +0000

    bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :offer
+
+  enum confirmation: { pending: 10, accepted: 20, declined: 30 }
 end
--- db/migrate/20240130141536_create_bookings.rb
+++ db/migrate/20240130141536_create_bookings.rb
@@ -2,7 +2,7 @@ class CreateBookings < ActiveRecord::Migration[7.1]
   def change
     create_table :bookings do |t|
       t.date :date
-      t.integer :confirmation
+      t.integer :confirmation, default: 0
       t.references :user, null: false, foreign_key: true
       t.references :offer, null: false, foreign_key: true
 
