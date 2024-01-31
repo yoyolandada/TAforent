0b2beba4fe4be1ed157bf7536483562a0b060676
Stamatis Vogiatzopoulos <stamvogiatzop@gmail.com>
2024-01-31 01:39:03 +0100

    index new create show pages back-end

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,34 @@
 class OffersController < ApplicationController
+  before_action :set_offer, only: [:show]
+
+  def index
+    @offers = Offer.all
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.user = current_user
+    if @offer.save
+      redirect_to offer_path(@offer)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_offer
+    @offer = Offer.find(params[:id])
+  end
+
+  def offer_params
+    params.require(:offer).permit(:specialty, :price, :availble)
+  end
 end
