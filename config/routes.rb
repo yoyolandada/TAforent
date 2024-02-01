c6e909e781110436596566791d6eecd473b23e7c
yolanda martinez priego <yulielx@gmail.com>
2024-02-01 15:03:08 +0000

    added links to accept/refuse booking. Tobe changed to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   end
 
   patch "bookings/:id/accept", to: "bookings#accept", as: :accept_booking
-  patch "bookings/:id/decline", to: "bookings#decline", as: :deline_booking
+  patch "bookings/:id/decline", to: "bookings#decline", as: :decline_booking
 end
