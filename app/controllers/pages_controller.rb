8177d9075b8f7017655efa12380f66ae27b2854c
Stamatis Vogiatzopoulos <stamvogiatzop@gmail.com>
2024-02-02 13:00:50 +0100

    navbar connection with dashboard, search method done, change card title in show and index and minor changes in front end such as the scss file text-decoration:none

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @specialties =  Offer.all.map { |offer| offer.specialty }.uniq
+    @specialties_option = []
+    @specialties.each_with_index do |specialty, index|
+      @specialties_option << [specialty, specialty]
+    end
   end
 
   def dashboard
